80092be1bf3fa0b95a7dde6149fec60e96924183
EMJOGARI <ejgameror@gmail.com>
2021-08-27 05:53:02 -0400

    modulo de cobranza terminado la vista

---
+++ resources/views/cobranzas/partials/data-factura/data-procesada.blade.php
@@ -0,0 +1,235 @@
+<div class="row">
+    <div class="col-md-12 form-inline mb-4">
+        <div class="form-group">
+            <h5><b>GUIAS INGRESO MATERIA PRIMA:</b></h5>
+            <input type="text" class="form-control ml-2" value="001-00006147" name="" readonly style="text-align: center; color: red;">
+        </div>
+    </div>   
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha ingreso materia prima:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="20 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha inicio procesamiento:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="21 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha término procesamiento:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="22 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha embarque cajas:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="23 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha llegada cajas</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="-----" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad"> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 form-inline mb-4">
+        <div class="form-group">
+            <h5><b>GUIAS INGRESO MATERIA PRIMA:</b></h5>
+            <input type="text" class="form-control ml-2" value="001-00006148" name="" readonly style="text-align: center; color: red;">
+        </div>
+    </div>   
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha ingreso materia prima:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="20 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha inicio procesamiento:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="21 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha término procesamiento:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="22 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha embarque cajas:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="23 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha llegada cajas</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="-----" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad"> Terminado</label>
+            </div>
+        </div> 
+    </div>
+    <div class="col-md-12 form-inline mb-4">
+        <div class="form-group">
+            <h5><b>GUIAS INGRESO MATERIA PRIMA:</b></h5>
+            <input type="text" class="form-control ml-2" value="001-00006149" name="" readonly style="text-align: center; color: red;">
+        </div>
+    </div>   
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha ingreso materia prima:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="20 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha inicio procesamiento:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="21 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha término procesamiento:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="22 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha embarque cajas:</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="23 junio 2021" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad" checked> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12 mb-4">              
+        <div class="row">  
+            <div class="col-md-4">
+                <h5><b>Fecha llegada cajas</b></h5>
+            </div>
+            <div class="col-md-4">
+                <input type="text" class="form-control form-control-sm ml-2 text-center" value="-----" name="" readonly>
+            </div>
+            <div class="col-md-4">
+                <label><input type="checkbox" name="contabilidad"> Terminado</label>
+            </div>
+        </div> 
+    </div>
+
+    <div class="col-md-12">
+        <div class="form-group">
+            <label>Observaciones:</label>
+            <textarea class="form-control" rows="3" placeholder="Enter ..." >Se coordinó con el cliente para la cancelacion de la factura venvida. Pago adelantado de US$ 1,500.00. Se comprometió en pagar el saldo final en una semana</textarea>
+        </div>
+    </div>
+</div>
